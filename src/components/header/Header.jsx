b8d838ee3348cca5a3f06bd639718052a54848ec
Adilxa <adilhaimerov82@gmail.com>
2022-04-11 13:31:28 +0600

    Adaptive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import classes from "./Header.module.css"
 const Header = (props) => {
     return (
         <div className={classes.wrapper}>
-            header({props.count})/({props.countTwo})
+            Todoes({props.count})
         </div>
     );
 }
